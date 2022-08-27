ca1d5a95d29cc04cf9602fb2421c4c5e49b61d01
Fray León Osorio Rivera <frayosorio@gmail.com>
2022-08-27 10:33:50 -0500

    Se agrega el controlador para Pais

--- src/main/java/tiendavideo/apitiendavideo/controlador/PaisControlador.java
+++ src/main/java/tiendavideo/apitiendavideo/controlador/PaisControlador.java
@@ -1,5 +1,40 @@
 package tiendavideo.apitiendavideo.controlador;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import tiendavideo.apitiendavideo.repositorio.PaisRepositorio;
+
+import tiendavideo.apitiendavideo.modelo.Pais;
+
+@RestController
+@RequestMapping("/paises")
 public class PaisControlador {
-    
+
+    @Autowired
+    private PaisRepositorio repositorio;
+
+    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    public List<Pais> listar() {
+        return repositorio.findAll();
+    }
+
+    @RequestMapping(value = "/obtener/{id}", method = RequestMethod.GET)
+    public Pais obtener(@PathVariable long id) {
+        return repositorio.findById(id).get();
+    }
+
+    @RequestMapping(value = "/agregar", method = RequestMethod.POST)
+    public Pais crear(@RequestBody Pais pais) {
+        return repositorio.save(pais);
+    }
+
+    @RequestMapping(value = "/modificar", method = RequestMethod.PUT)
+    public Pais actualizar(@RequestBody Pais pais) {
+        return repositorio.save(pais);
+    }
+
+
 }
--- src/main/java/tiendavideo/apitiendavideo/modelo/Pais.java
+++ src/main/java/tiendavideo/apitiendavideo/modelo/Pais.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 @Table(name = "pais")
 public class Pais {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
     @Column(name = "pais", length = 100, unique = true)
